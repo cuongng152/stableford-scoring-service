130f080e46af4f3def4e7c8658b34cf76d7db832
COSN\charlie <charlie@connective.com.au>
2023-01-18 16:52:15 +1100

    Update new attribute in course score endpoint

--- src/main/java/com/example/stablefordscoringservice/entity/CourseScore.java
+++ src/main/java/com/example/stablefordscoringservice/entity/CourseScore.java
@@ -27,13 +27,21 @@ public class CourseScore {
     @Column(name = "daily_handicap", nullable = false)
     private Integer dailyHandicap;
 
+    @Column(name = "average_driver_distance", nullable = false)
+    private Double avgDriverDistance;
+
+    @Column(name = "stableford_score", nullable = false)
+    private Integer stablefordScore;
+
     public CourseScore() {
     }
 
-    public CourseScore(Integer stroke, String courseName, Timestamp dateOfPlay, Integer dailyHandicap) {
+    public CourseScore(Integer stroke, String courseName, Timestamp dateOfPlay, Integer dailyHandicap, Double avgDriverDistance, Integer stablefordScore) {
         this.stroke = stroke;
         this.courseName = courseName;
         this.dateOfPlay = dateOfPlay;
         this.dailyHandicap = dailyHandicap;
+        this.avgDriverDistance = avgDriverDistance;
+        this.stablefordScore = stablefordScore;
     }
 }

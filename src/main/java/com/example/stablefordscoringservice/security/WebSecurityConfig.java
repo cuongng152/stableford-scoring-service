2403edf7c03f360ea3c54d6f623aa7b7d809edb1
COSN\charlie <charlie@connective.com.au>
2023-02-23 13:56:37 +1100

    Configure CORS

--- src/main/java/com/example/stablefordscoringservice/security/WebSecurityConfig.java
+++ src/main/java/com/example/stablefordscoringservice/security/WebSecurityConfig.java
@@ -5,10 +5,24 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig {
+public class WebSecurityConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+
+        registry.addMapping("/api/**")
+                .allowedOrigins("https://master.dg1gxs71ljkjp.amplifyapp.com", "https://127.0.0.1:3000")
+                .allowedMethods("PUT", "POST", "GET")
+                .allowedHeaders("header1", "header2", "header3")
+                .exposedHeaders("header1", "header2")
+                .allowCredentials(true).maxAge(3600);
+
+        // Add more mappings...
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
